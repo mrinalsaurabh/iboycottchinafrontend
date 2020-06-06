52610ef8eb9711448785e21fabd005817b0a4fab
Mrinal Saurabh <msaurabh@equalexperts.com>
2020-06-07 00:09:52 +0530

    Remove Sizes. Its not clothes.

--- src/pages/productOverview/components/Variants.js
+++ src/pages/productOverview/components/Variants.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from '../stylesheets/variants.module.sass'
-import Sizes from '../components/Sizes'
 
 
 export default function Variants({ color, size, variants, selectedSize, handleClick, clickSize }) {
@@ -16,18 +15,6 @@ export default function Variants({ color, size, variants, selectedSize, handleCl
         {variants && variants.map(v =>
           <img onClick={() => handleClick(v)} key={v.color} src={v.imagePath} alt="" />
         )}
-      </div>
-      <div className={styles.sizes}>
-        <div className={styles.size_title}>
-          SIZES:
-        </div>
-        <div className={styles.size_name}>
-          <Sizes
-            sizes={size.split(",")}
-            selectedSize={selectedSize}
-            clickSize={clickSize}
-          />
-        </div>
       </div>
     </div>
   )
